e1443288390dbcef8bb10d5854fab4c0530c9d81
Mary <101870828+Chioma227@users.noreply.github.com>
2024-06-09 22:30:09 +0100

    feat: new commit

--- components/organisms/home/Steps.tsx
+++ components/organisms/home/Steps.tsx
@@ -236,7 +236,7 @@ const Steps = () => {
                     </motion.div>
                     <motion.div
                         ref={ref}
-                        initial="hidden"
+                        initial="hidden" 
                         animate={controls}
                         variants={{
                             visible: { opacity: 1, x: 0 },
