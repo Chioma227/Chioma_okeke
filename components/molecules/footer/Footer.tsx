9a38e4c31e843743f495a0314de24fa5f93b478f
Mary <101870828+Chioma227@users.noreply.github.com>
2024-06-12 23:48:54 +0100

    style: added color to footer texts

--- components/molecules/footer/Footer.tsx
+++ components/molecules/footer/Footer.tsx
@@ -1,18 +1,14 @@
 "use client"
 import Button from '@/components/atoms/button'
 import { ButtonVariant } from '@/variants/rootVariants'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const Footer = () => {
-    let timeOfDay;
-    let timeHour = new Date().getHours()
-    const timeMinute = new Date().getMinutes()
-
-    if(timeHour < 12){
-        timeOfDay = "am"
-    }else{
-        timeOfDay = "pm"
-    }
+    const timeOfDay = new Date().toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true,
+    })
     return (
         <footer className='w-full footer'>
             <section className='secOne text-center flex items-center justify-center flex-col'>
@@ -21,9 +17,9 @@ const Footer = () => {
                 <p className='textthree'>Translating design ideas into visually appealing websites and enhancing performance.</p>
                 <Button variant={ButtonVariant.DEFAULT}>GET IN TOUCH</Button>
             </section>
-            <section className='flex items-center justify-between border border-t-2 border-opacity-5 border-yellow-50 py-[20px] px-[10px]'>
+            <section className='flex items-center justify-between border border-t-2 border-opacity-5 border-yellow-50 py-[20px] px-[10px] text-wheat-bold'>
                 <small className='text-[10px]'>&copy; 2024. Made By Chioma Okeke.</small>
-                <p className='text-[10px]'>{`${timeHour} : ${timeMinute} ${timeOfDay}`} </p>
+                <p className='text-[10px]'>{timeOfDay}</p>
             </section>
         </footer>
     )
